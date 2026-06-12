5312e3d70884fe0dad880188e447d78e2a4e95ce
ArsenyoP <arsenyo198510@gmail.com>
2026-06-12 22:49:04 +0300

    Remove unused assets and the counter functionality; implement product filtering and error handling in main.ts.

--- project/src/main.ts
+++ project/src/main.ts
@@ -1,60 +1,30 @@
-import './style.css'
-import typescriptLogo from './assets/typescript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.ts'
+//2
+const products = [
+  { id: 1, name: "Laptop", price: 1200, isAvailable: true },
+  { id: 2, name: "Mouse", price: 25, isAvailable: false },
+  { id: 3, name: "Keyboard", price: 80, isAvailable: true },
+  { id: 4, name: "Monitor", price: 300, isAvailable: true }
+];
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${typescriptLogo}" class="framework" alt="TypeScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.ts</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
+const result = products.filter(x => x.price > 50 && x.isAvailable === true)
+  .map(x => x.name)
 
-<div class="ticks"></div>
+console.log(result)
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src="${viteLogo}" alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://www.typescriptlang.org" target="_blank">
-          <img class="button-icon" src="${typescriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
+//3
+const apiErrors: Record<number, string> = {
+    400: "Некоректний запит",
+    401: "Неавторизовано",
+    404: "Ресурс не знайдено",
+    500: "Помилка сервера"
+}
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`
+function showErrorMessage(code:number): string{
+  return apiErrors[code] ?? "Невідомо"
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+let first = showErrorMessage(404)
+let second = showErrorMessage(323)
+
+console.log(`"Eroor 1: ${first}"`)
+console.log(`"Eroor 2: ${second}"`)
