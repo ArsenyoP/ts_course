e4e860df5cdd8bf6ad9fad2826bbcc2dc0633060
ArsenyoP <arsenyo198510@gmail.com>
2026-06-13 15:57:16 +0300

    AI task 1

--- project/src/main.ts
+++ project/src/main.ts
@@ -1,30 +1,24 @@
-//2
-const products = [
-  { id: 1, name: "Laptop", price: 1200, isAvailable: true },
-  { id: 2, name: "Mouse", price: 25, isAvailable: false },
-  { id: 3, name: "Keyboard", price: 80, isAvailable: true },
-  { id: 4, name: "Monitor", price: 300, isAvailable: true }
-];
-
-const result = products.filter(x => x.price > 50 && x.isAvailable === true)
-  .map(x => x.name)
-
-console.log(result)
-
-//3
-const apiErrors: Record<number, string> = {
-    400: "Некоректний запит",
-    401: "Неавторизовано",
-    404: "Ресурс не знайдено",
-    500: "Помилка сервера"
+enum Status {
+  Pending,
+  Success,
+  Cancelled
 }
 
-function showErrorMessage(code:number): string{
-  return apiErrors[code] ?? "Невідомо"
+type Transaction = [string, number, Status]
+
+let history: Array<Transaction> = [
+  ["tx_001", 10.4, Status.Success],
+  ["tx_002", 142.25, Status.Success],
+  ["tx_003", 4265.42, Status.Cancelled]
+]
+
+function getTotalSuccessAmount(txList: Transaction[]): number{
+  let result = txList.filter(x => x[2] == Status.Success)
+    .map(x => x[1])
+    .reduce((sum, current) => sum + current, 0);  
+    
+    return result;
 }
 
-let first = showErrorMessage(404)
-let second = showErrorMessage(323)
-
-console.log(`"Eroor 1: ${first}"`)
-console.log(`"Eroor 2: ${second}"`)
+var summ = getTotalSuccessAmount(history);
+console.log(summ);
