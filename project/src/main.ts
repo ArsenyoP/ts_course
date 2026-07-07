75108226e8242862068a404a97c7fdad3d5d535e
ArsenyoP <arsenyo198510@gmail.com>
2026-07-07 23:34:51 +0300

    rest params and overload

--- project/src/main.ts
+++ project/src/main.ts
@@ -1,22 +1,60 @@
-const notifyUser = (text: string, deliveryMethod: ( msg: string) => void): void => {
-    deliveryMethod(text)
+const logActivity = (action: string, ...products: string[]): void => 
+    {
+        if(products.length === 0)
+        {
+            console.log(`Action without changes: ${action}`)
+        }
+
+        const changedProducts = products.join(", ");
+        console.log(`Action: ${action}, affected products: ${products}`)
 }
 
-const result = notifyUser("Some text", msg => console.log("[EMAIL] " + msg))
-const result2 = notifyUser("Some text from SMS", msg => console.log("[SMS] " + msg))
-
-result2
-result
+logActivity("View", "Iphone","AirPods","Case")
 
 
 
-const modifyText = (text: string, transformer: (t: string) => string ): string => {
-    const result = transformer(text);
-    return result;
+
+function getUser(userName: string): string;
+function getUser(userId: number): string;
+
+function getUser(IdOrUsername: string | number): string
+{
+    if(typeof IdOrUsername === "string")
+    {
+        return `Username search: ${IdOrUsername}`
+    }
+    else if(typeof IdOrUsername === "number")
+    {
+        return `Id search: ${IdOrUsername}`
+    }
 }
 
-const transformResultUpper = modifyText("Text", text => text.toUpperCase())
-const transformResultStars = modifyText("Text", text => `***${text}***`)
 
-console.log(transformResultStars)
-console.log(transformResultUpper)
+console.log(getUser("Arsen"))
+console.log(getUser(173))
+
+
+
+function combine(...numbers: number[]): number;
+function combine(...strings: string[]): string;
+
+function combine(...numbsOrStrings: number[] | string[]): number | string
+{
+    if(typeof numbsOrStrings[0] === "string")
+    {
+        return (numbsOrStrings as string[]).join(" ")
+    }
+
+    let sum: number = 0;
+    for(let i: number = 0; i < numbsOrStrings.length; i++)
+    {
+        sum += (numbsOrStrings as number[])[i]
+    }
+    return sum;
+}
+
+const resultCombine1 = combine(1,3,64,5,245)
+console.log(resultCombine1)
+
+const resultCombine2 = combine("Some", "text", "to", "combine")
+console.log(resultCombine2)
