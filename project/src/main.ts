fe9dd10a095ec1cb345ec631694ca2af49f5b3e1
ArsenyoP <arsenyo198510@gmail.com>
2026-07-13 21:09:07 +0300

    Generic task

--- project/src/main.ts
+++ project/src/main.ts
@@ -1,35 +1,31 @@
-interface TextMessage{
-    message: string,
-    sender: string
+type PaginationMeta = {
+    totalCOunt: number,
+    page: number,
+    pageSize: number
 }
 
-interface SystemMessage{
-    event: string,
-    code: number
+type ApiResponse<T> = PaginationMeta & {items: T[]}
+
+
+interface Book{
+    author: string,
+    title: string
 }
 
-type AppMessage = TextMessage | SystemMessage
-
-function parseMessage(msg: AppMessage): void{
-    if("message" in msg)
-    {
-        console.log(`User: ${msg.sender} send message: ${msg.message}`)
-    }
-    else if("event" in msg)
-    {
-        console.log(`Event: ${msg.event} with code: ${msg.code}`)
-    }
-} 
-
-const textMessage: TextMessage = {
-    message: "Some message",
-    sender: "Arsen"
-} 
-
-const systemMessage: SystemMessage = {
-    event: "Some event",
-    code: 404
+const book1: Book = {
+    author: "J.K Rouling",
+    title: "Harry Potter"
+}
+const book2: Book = {
+    author: "Author",
+    title: "Title"
 }
 
-parseMessage(textMessage)
-parseMessage(systemMessage)
+const response: ApiResponse<Book> = {   
+    totalCOunt: 100,
+    page: 2,
+    pageSize: 20,
+    items: [book1, book2]
+}
+
+console.log(`Total: ${response.totalCOunt}, first title: ${response.items[0].title}`)
